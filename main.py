dde3fa429b3475b874f1c7881b2f468a06eb1f63
Phillip Hansen <phille04@outlook.com>
2022-02-28 21:24:03 +0100

    More or less finished the alpha version

--- main.py
+++ main.py
@@ -29,6 +29,8 @@ def MainMenu():
     print()
     print(" 11 - Generate a full Character")
     print()
+    print(" 12 - Display your generated character's full information")
+    print()
     choice = input(" Which option would you like to choose? ")
     if choice == "1":
         RandomizeAbilities()
@@ -61,12 +63,16 @@ def MainMenu():
         RandomizeStaminaAll()
         RandomizeRangeAll()
         RandomizeWeaknessAll()
+    elif choice == "12":
+        DisplayFullStats()
     else:
         print(" Incorrect choice, try again!")
         time.sleep(1)
         MainMenu()
 
 def RandomizeAbilities():
+    global GeneratedAbilitiesList
+    GeneratedAbilitiesList = []
     NumberOfAbilities = int(input(" How many abilities do you want your character to have? (1-100, leave 0 for random) "))
     if NumberOfAbilities == 0:
         NumberOfAbilities = random.randint(0,101)
@@ -77,84 +83,95 @@ def RandomizeAbilities():
     while GeneratedAbilities < NumberOfAbilities:
         RandomizedAbility = random.choice(ListOfTiers)
         ListOfTiers.remove(RandomizedAbility)
-        print(" Your character's Ability is: ", RandomizedAbility)
+        print(" Your character's Ability is:", RandomizedAbility)
+        GeneratedAbilitiesList.append(RandomizedAbility)
         GeneratedAbilities += 1
-    time.sleep(3)
+    time.sleep(2)
     MainMenu()
 
 def RandomizeStrength():
+    global RandomizedStrength
     StrengthTiersDictionary = open("Strength Tiers.txt", "r")
     StrengthTiers = StrengthTiersDictionary.read()
     ListOfTiers = StrengthTiers.split(",")
-    RandomizedTier = random.choice(ListOfTiers)
-    print(" Your character's Physical Strength is: ", RandomizedTier)
+    RandomizedStrength = random.choice(ListOfTiers)
+    print(" Your character's Physical Strength is:", RandomizedStrength)
     time.sleep(1)
     MainMenu()
 
 def RandomizeDestruction():
+    global RandomizedDestruction
     DestructionTiersDictionary = open("Strength Tiers.txt", "r")
     DestructionTiers = DestructionTiersDictionary.read()
     ListOfTiers = DestructionTiers.split(",")
-    RandomizedTier = random.choice(ListOfTiers)
-    print(" Your character's Attack Potency/Destructive Capacity is: ", RandomizedTier)
+    RandomizedDestruction = random.choice(ListOfTiers)
+    print(" Your character's Attack Potency/Destructive Capacity is:", RandomizedDestruction)
     time.sleep(1)
     MainMenu()
 
 def RandomizeDurability():
+    global RandomizedDurability
     DurabilityTiersDictionary = open("Strength Tiers.txt", "r")
     DurabilityTiers = DurabilityTiersDictionary.read()
     ListOfTiers = DurabilityTiers.split(",")
-    RandomizedTier = random.choice(ListOfTiers)
-    print(" Your character's Durability is: ", RandomizedTier)
+    RandomizedDurability = random.choice(ListOfTiers)
+    print(" Your character's Durability is:", RandomizedDurability)
     time.sleep(1)
     MainMenu()
 
 def RandomizeMoveSpeed():
+    global RandomizedMoveSpeed
     MoveSpeedTiersDictionary = open("Speed Tiers.txt", "r")
     MoveSpeedTiers = MoveSpeedTiersDictionary.read()
     ListOfTiers = MoveSpeedTiers.split(",")
-    RandomizedTier = random.choice(ListOfTiers)
-    print(" Your character's Movement Speed is: ", RandomizedTier)
+    RandomizedMoveSpeed = random.choice(ListOfTiers)
+    print(" Your character's Movement Speed is:", RandomizedMoveSpeed)
     time.sleep(1)
     MainMenu()
 
 def RandomizeReactionSpeed():
+    global RandomizedReactSpeed
     ReactSpeedTiersDictionary = open("Speed Tiers.txt", "r")
     ReactSpeedTiers = ReactSpeedTiersDictionary.read()
     ListOfTiers = ReactSpeedTiers.split(",")
-    RandomizedTier = random.choice(ListOfTiers)
-    print(" Your character's Reaction Speed is: ", RandomizedTier)
+    RandomizedReactSpeed = random.choice(ListOfTiers)
+    print(" Your character's Reaction Speed is:", RandomizedReactSpeed)
     time.sleep(1)
     MainMenu()
 
 def RandomizeIntelligence():
+    global RandomizedIntelligence
     IntelligenceTiersDictionary = open("Intelligence Tiers.txt", "r")
     IntelligenceTiers = IntelligenceTiersDictionary.read()
     ListOfTiers = IntelligenceTiers.split(",")
-    RandomizedTier = random.choice(ListOfTiers)
-    print(" Your character's Intelligence is: ", RandomizedTier)
+    RandomizedIntelligence = random.choice(ListOfTiers)
+    print(" Your character's Intelligence is:", RandomizedIntelligence)
     time.sleep(1)
     MainMenu()
 
 def RandomizeStamina():
+    global RandomizedStamina
     StaminaTiersDictionary = open("Stamina Tiers.txt", "r")
     StaminaTiers = StaminaTiersDictionary.read()
     ListOfTiers = StaminaTiers.split(",")
-    RandomizedTier = random.choice(ListOfTiers)
-    print(" Your character's Stamina is: ", RandomizedTier)
+    RandomizedStamina = random.choice(ListOfTiers)
+    print(" Your character's Stamina is:", RandomizedStamina)
     time.sleep(1)
     MainMenu()
 
 def RandomizeRange():
+    global RandomizedRange
     RangeTiersDictionary = open("Range Tiers.txt", "r")
     RangeTiers = RangeTiersDictionary.read()
     ListOfTiers = RangeTiers.split(",")
-    RandomizedTier = random.choice(ListOfTiers)
-    print(" Your character's Range is: ", RandomizedTier)
+    RandomizedRange = random.choice(ListOfTiers)
+    print(" Your character's Range is:", RandomizedRange)
     time.sleep(1)
     MainMenu()
 
 def RandomizeWeakness():
+    global GeneratedWeaknessesList
+    GeneratedWeaknessesList = []
     NumberOfWeaknesses = random.randint(0,4)
     GeneratedWeaknesses = 0
     WeaknessDictionary = open("Weaknesses.txt", "r")
@@ -166,15 +183,18 @@ def RandomizeWeakness():
         while GeneratedWeaknesses < NumberOfWeaknesses:
             RandomizedWeaknesses = random.choice(ListOfTiers)
             ListOfTiers.remove(RandomizedWeaknesses)
-            print(" Your character's Weakness is: ", RandomizedWeaknesses)
+            print(" Your character's Weakness is:", RandomizedWeaknesses)
+            GeneratedWeaknessesList.append(RandomizedWeaknesses)
             GeneratedWeaknesses += 1
     time.sleep(1)
     MainMenu()
 
 def RandomizeAbilitiesAll():
+    global GeneratedAbilitiesList
+    GeneratedAbilitiesList = []
     NumberOfAbilities = int(input(" How many abilities do you want your character to have? (1-100, leave 0 for random) "))
     if NumberOfAbilities == 0:
-        NumberOfAbilities = random.randint(1,100)
+        NumberOfAbilities = random.randint(0,101)
     GeneratedAbilities = 0
     AbilitiesDictionary = open("Abilities.txt", "r")
     Abilities = AbilitiesDictionary.read()
@@ -182,75 +202,86 @@ def RandomizeAbilitiesAll():
     while GeneratedAbilities < NumberOfAbilities:
         RandomizedAbility = random.choice(ListOfTiers)
         ListOfTiers.remove(RandomizedAbility)
-        print(" Your character's Ability is: ", RandomizedAbility)
+        print(" Your character's Ability is:", RandomizedAbility)
+        GeneratedAbilitiesList.append(RandomizedAbility)
         GeneratedAbilities += 1
-    time.sleep(3)
+    time.sleep(2)
 
 def RandomizeStrengthAll():
+    global RandomizedStrength
     StrengthTiersDictionary = open("Strength Tiers.txt", "r")
     StrengthTiers = StrengthTiersDictionary.read()
     ListOfTiers = StrengthTiers.split(",")
-    RandomizedTier = random.choice(ListOfTiers)
-    print(" Your character's Physical Strength is: ", RandomizedTier)
+    RandomizedStrength = random.choice(ListOfTiers)
+    print(" Your character's Physical Strength is:", RandomizedStrength)
     time.sleep(1)
 
 def RandomizeDestructionAll():
+    global RandomizedDestruction
     DestructionTiersDictionary = open("Strength Tiers.txt", "r")
     DestructionTiers = DestructionTiersDictionary.read()
     ListOfTiers = DestructionTiers.split(",")
-    RandomizedTier = random.choice(ListOfTiers)
-    print(" Your character's Attack Potency/Destructive Capacity is: ", RandomizedTier)
+    RandomizedDestruction = random.choice(ListOfTiers)
+    print(" Your character's Attack Potency/Destructive Capacity is:", RandomizedDestruction)
     time.sleep(1)
 
 def RandomizeDurabilityAll():
+    global RandomizedDurability
     DurabilityTiersDictionary = open("Strength Tiers.txt", "r")
     DurabilityTiers = DurabilityTiersDictionary.read()
     ListOfTiers = DurabilityTiers.split(",")
-    RandomizedTier = random.choice(ListOfTiers)
-    print(" Your character's Durability is: ", RandomizedTier)
+    RandomizedDurability = random.choice(ListOfTiers)
+    print(" Your character's Durability is:", RandomizedDurability)
     time.sleep(1)
 
 def RandomizeMoveSpeedAll():
+    global RandomizedMoveSpeed
     MoveSpeedTiersDictionary = open("Speed Tiers.txt", "r")
     MoveSpeedTiers = MoveSpeedTiersDictionary.read()
     ListOfTiers = MoveSpeedTiers.split(",")
-    RandomizedTier = random.choice(ListOfTiers)
-    print(" Your character's Movement Speed is: ", RandomizedTier)
+    RandomizedMoveSpeed = random.choice(ListOfTiers)
+    print(" Your character's Movement Speed is:", RandomizedMoveSpeed)
     time.sleep(1)
 
 def RandomizeReactionSpeedAll():
+    global RandomizedReactSpeed
     ReactSpeedTiersDictionary = open("Speed Tiers.txt", "r")
     ReactSpeedTiers = ReactSpeedTiersDictionary.read()
     ListOfTiers = ReactSpeedTiers.split(",")
-    RandomizedTier = random.choice(ListOfTiers)
-    print(" Your character's Reaction Speed is: ", RandomizedTier)
+    RandomizedReactSpeed = random.choice(ListOfTiers)
+    print(" Your character's Reaction Speed is:", RandomizedReactSpeed)
     time.sleep(1)
 
 def RandomizeIntelligenceAll():
+    global RandomizedIntelligence
     IntelligenceTiersDictionary = open("Intelligence Tiers.txt", "r")
     IntelligenceTiers = IntelligenceTiersDictionary.read()
     ListOfTiers = IntelligenceTiers.split(",")
-    RandomizedTier = random.choice(ListOfTiers)
-    print(" Your character's Intelligence is: ", RandomizedTier)
+    RandomizedIntelligence = random.choice(ListOfTiers)
+    print(" Your character's Intelligence is:", RandomizedIntelligence)
     time.sleep(1)
 
 def RandomizeStaminaAll():
+    global RandomizedStamina
     StaminaTiersDictionary = open("Stamina Tiers.txt", "r")
     StaminaTiers = StaminaTiersDictionary.read()
     ListOfTiers = StaminaTiers.split(",")
-    RandomizedTier = random.choice(ListOfTiers)
-    print(" Your character's Stamina is: ", RandomizedTier)
+    RandomizedStamina = random.choice(ListOfTiers)
+    print(" Your character's Stamina is:", RandomizedStamina)
     time.sleep(1)
 
 def RandomizeRangeAll():
+    global RandomizedRange
     RangeTiersDictionary = open("Range Tiers.txt", "r")
     RangeTiers = RangeTiersDictionary.read()
     ListOfTiers = RangeTiers.split(",")
-    RandomizedTier = random.choice(ListOfTiers)
-    print(" Your character's Range is: ", RandomizedTier)
+    RandomizedRange = random.choice(ListOfTiers)
+    print(" Your character's Range is:", RandomizedRange)
     time.sleep(1)
 
 def RandomizeWeaknessAll():
+    global GeneratedWeaknessesList
+    GeneratedWeaknessesList = []
     NumberOfWeaknesses = random.randint(0,4)
     GeneratedWeaknesses = 0
     WeaknessDictionary = open("Weaknesses.txt", "r")
@@ -262,9 +293,31 @@ def RandomizeWeaknessAll():
         while GeneratedWeaknesses < NumberOfWeaknesses:
             RandomizedWeaknesses = random.choice(ListOfTiers)
             ListOfTiers.remove(RandomizedWeaknesses)
-            print(" Your character's Weakness is: ", RandomizedWeaknesses)
+            print(" Your character's Weakness is:", RandomizedWeaknesses)
+            GeneratedWeaknessesList.append(RandomizedWeaknesses)
             GeneratedWeaknesses += 1
     time.sleep(1)
+    MainMenu()
+
+def DisplayFullStats():
+    #AbilityList = ", ".join(GeneratedAbilitiesList)
+    #WeaknessList = ", ".join(GeneratedWeaknessesList)
+    print()
+    CharacterName = input(" Name you character: ")
+    print()
+    print(" The stats of your character", CharacterName, "are as follows:")
+    print()
+    print(" Your characters Abilities are", GeneratedAbilitiesList)
+    print(" Your characters Physical Strength is", RandomizedStrength)
+    print(" Your characters Attack Potency/Destructive Capacity is", RandomizedDestruction)
+    print(" Your characters Durability is", RandomizedDurability)
+    print(" Your characters Movement Speed is", RandomizedMoveSpeed)
+    print(" Your characters Reaction Speed is", RandomizedReactSpeed)
+    print(" Your characters Intelligence is", RandomizedIntelligence)
+    print(" Your characters Stamina is", RandomizedStamina)
+    print(" Your characters Range is", RandomizedRange)
+    print(" Your characters Weaknesses are ", GeneratedWeaknessesList)
+    time.sleep(10)
     MainMenu()
 
 MainMenu()
